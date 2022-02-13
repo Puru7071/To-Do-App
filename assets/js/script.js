98454f04334cdb584a479a7aa8003d6d07523bc8
Puru Bhargava <purubhargava011@gmail.com>
2022-02-13 13:22:37 +0530

    Added deadline bar and made few change in the front end part

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,46 @@
 (function(){
-console.log("Script Loaded!!") ; 
+    console.log("Script Loaded!!") ; 
+    var tasks = null ; 
 
-var socialMediaIcons = document.getElementsByClassName("socialMediaIcons") ; 
-console.log(socialMediaIcons) ; 
-
-for(let i = 0 ; i < socialMediaIcons.length ; i += 1){
-    socialMediaIcons[i].addEventListener("mouseover" , function(event){
-        event.stopPropagation() ; 
-
-        if(i == 0){
-            socialMediaIcons[i].style.color = "#0a66c2" ; 
+    taskList.find({} , function(error , docs){
+        if(error){
+            console.log("Internal Error !!") ; 
+            return ; 
         }
-        if(i == 1){
-            socialMediaIcons[i].style.color = "#c32aa3" ; 
-        }
-        if(i == 2){
-            socialMediaIcons[i].style.color = "#1b1f23" ; 
-        }
-        if(i == 3){
-            socialMediaIcons[i].style.color = "#f7f400" ; 
-        }
-       
-        return ; 
+        tasks = docs ; 
+        return ;   
     }) ; 
 
-    socialMediaIcons[i].addEventListener("mouseout" , function(event){
-        event.stopImmediatePropagation() ; 
-        socialMediaIcons[i].style.color = "#f1faee" ; 
+    console.log(tasks) ; 
+
+    var socialMediaIcons = document.getElementsByClassName("socialMediaIcons") ; 
+    console.log(socialMediaIcons) ; 
+
+    for(let i = 0 ; i < socialMediaIcons.length ; i += 1){
+        socialMediaIcons[i].addEventListener("mouseover" , function(event){
+            event.stopPropagation() ; 
+
+            if(i == 0){
+                socialMediaIcons[i].style.color = "#0a66c2" ; 
+            }
+            if(i == 1){
+                socialMediaIcons[i].style.color = "#c32aa3" ; 
+            }
+            if(i == 2){
+                socialMediaIcons[i].style.color = "#1b1f23" ; 
+            }
+            if(i == 3){
+                socialMediaIcons[i].style.color = "#f7f400" ; 
+            }
         
-    }) ; 
-}
-// var tick1 =  document.getElementsByClassName("far fa-square fa-2x") ; 
-// var tick2 = document.getElementsByClassName("far fa-check-square fa-2x") ; 
+            return ; 
+        }) ; 
 
-// for(let i = 0 ; i < tick1.length ; i += 1){
-//     tick1[i].addEventListener("click" , function(event){
-//         tick1[i].setAttribute("class" , "far fa-check-square fa-2x") ;
-//         tick2 = document.getElementsByClassName("far fa-check-square fa-2x") ; 
-//         tick1.splice(i , 1) ; 
-//     }) ; 
-// }
+        socialMediaIcons[i].addEventListener("mouseout" , function(event){
+            event.stopImmediatePropagation() ; 
+            socialMediaIcons[i].style.color = "#f1faee" ; 
+            
+        }) ; 
+    }
 
-// for(let i = 0 ; i < tick2.length ; i += 1){
-//     tick2[i].addEventListener("click" , function(event){
-//         tick2[i].setAttribute("class" , "far fa-square fa-2x") ; 
-//         tick1 =  document.getElementsByClassName("far fa-square fa-2x") ; 
-//         tick2.splice(i , 1) ; 
-//     }) ;
-// }
 })() ; 
--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,15 +1,34 @@
 const taskList = require("../models/TaskSchema") ; 
 
+function findMinDate(Tasks){
+    var taskDate = new Date("2080-12-12") ; 
+    var taskName = null ; 
+    for(let i = 0 ; i < Tasks.length ; i += 1){
+        var tempDate = new Date(Tasks[i].deadline) ; 
+        if(tempDate < taskDate){
+            taskDate = tempDate
+            taskName = Tasks[i].Name ; 
+        }
+    }
+    return [taskName , taskDate] ; 
+}
+
+
+
 module.exports.renderHomePage = function(request , response){
     taskList.find({}, function (error, Tasks) {
         if(error){
             console.log(`Opps !! Unable to Render the App due to: ${error}`) ; 
             return ; 
         }
+        
         let obj = {
             tasksToDo : Tasks , 
-            number : Tasks.length
+            number : Tasks.length , 
+            closestTask : findMinDate(Tasks)[0] , 
+            closestDate : findMinDate(Tasks)[1] 
         }
+        
         return response.render("home",obj) ; 
     });
 }
